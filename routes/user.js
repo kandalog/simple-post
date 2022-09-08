62bb9eb20196d126abf935cfb9ba0dd9858b9bc4
kandalog <asuasu.tee@gmail.com>
2022-09-09 04:14:57 +0900

    userのCRUD処理を実装

--- routes/user.js
+++ routes/user.js
@@ -16,12 +16,33 @@ router.get("/", async (req, res) => {
 // 特定のユーザーを取得
 router.get("/:id", async (req, res) => {
   try {
-    const user = await User.findOne({
-      where: {
-        id: req.params.id,
-      },
-    });
+    const user = await User.findByPk(req.params.id);
     return res.status(200).json(user);
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+});
+
+// ユーザーを更新する
+router.put("/:id", async (req, res) => {
+  try {
+    const user = await User.findByPk(req.params.id);
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    user.password = req.body.password || user.password;
+    await user.save();
+    return res.status(200).json(user);
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+});
+
+// ユーザーを削除する
+router.delete("/:id", async (req, res) => {
+  try {
+    const user = await User.findByPk(req.params.id);
+    await user.destroy();
+    return res.status(200).json("削除に成功しました");
   } catch (err) {
     return res.status(500).json(err);
   }
