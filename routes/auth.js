6898061c1a7d440e583c2f33edb1a4bf967b0640
kandalog <asuasu.tee@gmail.com>
2022-09-09 21:50:12 +0900

    bcryptを導入し、secureなログイン認証を実装

--- routes/auth.js
+++ routes/auth.js
@@ -1,20 +1,47 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcrypt");
 
 const { User } = require("../models");
 
 // 新規ユーザーを登録
 router.post("/", async (req, res) => {
   try {
+    const hash = await bcrypt.hash(req.body.password, 10);
     const user = await User.create({
       name: req.body.name,
       email: req.body.email,
-      password: req.body.password,
+      password: hash,
     });
     return res.status(200).json(user);
   } catch (err) {
     return res.status(500).json(err);
   }
+});
+
+// ログイン
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ where: { email: req.body.email } });
+    if (!user) {
+      return res.status(404).json("ユーザーが存在しません。");
+    }
+    const flag = await bcrypt.compare(req.body.password, user.password);
+    if (flag) {
+      req.session.current_user = user;
+      return res.status(200).json(user);
+    } else {
+      return res.status(403).json("パスワードが違います");
+    }
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+});
+
+// ログアウト
+router.get("/logout", (req, res) => {
+  req.session.login = undefined;
+  return res.status(200).json("ログアウトしました");
 });
 
 module.exports = router;
